2599a56a8dc607eadbe79067434bdd288ef4d642
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-10-14 18:46:19 +0200

    Ajoutez des fichiers projet.

---
+++ DISM Drv GUI/Main.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DISM_Drv_GUI
+{
+    public partial class Main : Form
+    {
+        //Set dark mode title bar and bypass wow64 redirection
+
+        [DllImport("DwmApi")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool Wow64DisableWow64FsRedirection(ref IntPtr ptr);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool Wow64RevertWow64FsRedirection(IntPtr ptr);
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            DwmSetWindowAttribute(Handle, 19, new[] { 1 }, 4);
+            DwmSetWindowAttribute(Handle, 20, new[] { 1 }, 4);
+            DwmSetWindowAttribute(Handle, 35, new[] { 1 }, 4);
+            DwmSetWindowAttribute(Handle, 38, new[] { 1 }, 4);
+            IntPtr wow64Value = IntPtr.Zero;
+            Wow64DisableWow64FsRedirection(ref wow64Value);
+        }
+
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void bb_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    tbb.Text = fbd.SelectedPath + "\\Drv_" + DateTime.Now.ToString("HH-mm_dd-MM-yy");
+                }
+            }
+        }
+
+        private void b_Click(object sender, EventArgs e)
+        {
+            if (tbb.Text == "")
+            {
+
+            }
+            else
+            {
+                Directory.CreateDirectory(tbb.Text);
+                string arg = "/online /export-driver /destination:" + "\"" + tbb.Text + "\"";
+
+                Process p = new Process();
+                p.StartInfo.FileName = "powershell.exe";
+                p.StartInfo.Arguments = arg;
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+                p.Start();
+                p.WaitForExit();
+
+                using(WebClient wc = new WebClient())
+                {
+                    wc.DownloadFile("https://raw.githubusercontent.com/STY1001/PS-Drv-GUI/master/Restore.exe", tbb.Text + "\\Restore.exe");
+                }
+            }
+        }
+
+        private void br_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    tbr.Text = fbd.SelectedPath + "\\Restore.exe";
+                }
+            }
+        }
+
+        private void r_Click(object sender, EventArgs e)
+        {
+            if (tbr.Text == "")
+            {
+
+            }
+            else
+            {
+                System.Diagnostics.Process.Start(tbr.Text);
+            }
+        }
+    }
+}
---
+++ DISM Drv GUI/Main.designer.cs
@@ -0,0 +1,164 @@
+﻿namespace DISM_Drv_GUI
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbb = new System.Windows.Forms.TextBox();
+            this.bb = new ReaLTaiizor.Controls.Button();
+            this.b = new ReaLTaiizor.Controls.Button();
+            this.tbr = new System.Windows.Forms.TextBox();
+            this.br = new ReaLTaiizor.Controls.Button();
+            this.r = new ReaLTaiizor.Controls.Button();
+            this.SuspendLayout();
+            // 
+            // tbb
+            // 
+            this.tbb.BackColor = System.Drawing.Color.Black;
+            this.tbb.ForeColor = System.Drawing.Color.White;
+            this.tbb.Location = new System.Drawing.Point(13, 13);
+            this.tbb.Name = "tbb";
+            this.tbb.ReadOnly = true;
+            this.tbb.Size = new System.Drawing.Size(354, 22);
+            this.tbb.TabIndex = 0;
+            // 
+            // bb
+            // 
+            this.bb.BackColor = System.Drawing.Color.Transparent;
+            this.bb.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bb.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.bb.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bb.Image = null;
+            this.bb.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bb.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.bb.Location = new System.Drawing.Point(310, 41);
+            this.bb.Name = "bb";
+            this.bb.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.bb.Size = new System.Drawing.Size(57, 26);
+            this.bb.TabIndex = 1;
+            this.bb.Text = "Explore";
+            this.bb.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.bb.Click += new System.EventHandler(this.bb_Click);
+            // 
+            // b
+            // 
+            this.b.BackColor = System.Drawing.Color.Transparent;
+            this.b.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.b.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.b.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b.Image = null;
+            this.b.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.b.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.b.Location = new System.Drawing.Point(310, 73);
+            this.b.Name = "b";
+            this.b.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.b.Size = new System.Drawing.Size(57, 26);
+            this.b.TabIndex = 1;
+            this.b.Text = "Backup";
+            this.b.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.b.Click += new System.EventHandler(this.b_Click);
+            // 
+            // tbr
+            // 
+            this.tbr.BackColor = System.Drawing.Color.Black;
+            this.tbr.ForeColor = System.Drawing.Color.White;
+            this.tbr.Location = new System.Drawing.Point(12, 105);
+            this.tbr.Name = "tbr";
+            this.tbr.ReadOnly = true;
+            this.tbr.Size = new System.Drawing.Size(354, 22);
+            this.tbr.TabIndex = 0;
+            // 
+            // br
+            // 
+            this.br.BackColor = System.Drawing.Color.Transparent;
+            this.br.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.br.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.br.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.br.Image = null;
+            this.br.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.br.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.br.Location = new System.Drawing.Point(309, 133);
+            this.br.Name = "br";
+            this.br.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.br.Size = new System.Drawing.Size(57, 26);
+            this.br.TabIndex = 1;
+            this.br.Text = "Explore";
+            this.br.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.br.Click += new System.EventHandler(this.br_Click);
+            // 
+            // r
+            // 
+            this.r.BackColor = System.Drawing.Color.Transparent;
+            this.r.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.r.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.r.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r.Image = null;
+            this.r.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.r.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.r.Location = new System.Drawing.Point(309, 165);
+            this.r.Name = "r";
+            this.r.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.r.Size = new System.Drawing.Size(57, 26);
+            this.r.TabIndex = 1;
+            this.r.Text = "Restore";
+            this.r.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.r.Click += new System.EventHandler(this.r_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(382, 215);
+            this.Controls.Add(this.r);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.br);
+            this.Controls.Add(this.bb);
+            this.Controls.Add(this.tbr);
+            this.Controls.Add(this.tbb);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "DISM Drv GUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbb;
+        private ReaLTaiizor.Controls.Button bb;
+        private ReaLTaiizor.Controls.Button b;
+        private System.Windows.Forms.TextBox tbr;
+        private ReaLTaiizor.Controls.Button br;
+        private ReaLTaiizor.Controls.Button r;
+    }
+}
+
